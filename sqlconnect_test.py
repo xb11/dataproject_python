ce86f6fac8113198c4b12487c39f75202ee6506e
Bo Xiong <wo635627885@163.com>
2014-07-23 05:03:19 -0400

    python_v2
    add stop calculation and painting column chart

--- sqlconnect_test.py
+++ sqlconnect_test.py
@@ -2,11 +2,12 @@
 #coding=utf-8
 import mysql.connector
 import numpy
+import matplotlib.pyplot as plt
 from mysql.connector import errorcode
 #Try to test if the connection is done
-#test change
-#user_id = "root"
-#password = "1234"
+
+user_id = "root"
+password = "1234"
 try:
   cnx = mysql.connector.connect(user='root',password='1234',database='dataproject')
 except mysql.connector.Error as err:
@@ -46,23 +47,57 @@ except mysql.connector.Error as err:
 startmonth = 10
 endmonth = 10
 starttime = 7
-endtime = \
-9
+endtime = 9
+stopA = 2
+stopB = 5
+route = 1
 
-def calculate_crowding(startmonth,endmonth,starttime,endtime):
-    sql = "SELECT `LOAD_NUM`,`capacity` FROM `dataproject`.`transit` \
-    WHERE %d<=HR AND HR<=%d AND '2013-%d-01'<=daymoyr AND daymoyr<='2013-%d-31'" \
-    % (starttime,endtime,startmonth,endmonth)
+
+def calculate_crowding(startmonth,endmonth,starttime,endtime,stopA,stopB,route):
+    sql = "SELECT `LOAD_NUM`,`capacity`,`STOPA` FROM `dataproject`.`transit` \
+    WHERE %d<=HR AND HR<=%d AND '2013-%d-01'<=daymoyr AND daymoyr<='2013-%d-31'\
+     AND ROUTE = %d AND %d < STOPA AND STOPA <= %d"\
+    % (starttime,endtime,startmonth,endmonth,route,stopA,stopB)
     cursor.execute(sql)
     results = cursor.fetchall()
-    crowding = []
+    number = stopB - stopA
+    crowding = [[] for i in range(number)]
     for row in results:
-        crowding.append(row[0]/30.0/row[1])
+        crowding[row[2]-stopA-1].append(row[0]/30.0/row[1])
     return crowding
 
-crowd = calculate_crowding(10,10,7,9)
-#print numpy.mean(crowd)
-print crowd[0:5]
+crowd = calculate_crowding(10,10,7,9,stopA,stopB,1)
+
+StopMeans   = []
+
+for i in range(stopA + 1, stopB+1):
+    StopMeans.append(numpy.mean(crowd[i-stopA-1]))
+    print "from stop %d to %d, the crowding is %f "%(i-1,i,StopMeans[i-stopA -1])
+
+#StopMeans = [0.174056,0.153506,0.164026]
+N = stopB - stopA
+max = numpy.max(StopMeans)
+#print max
+#print numpy.arange(0,max*1.33,max/6.0)
+#ticks = ['stop3','stop4','stop5']
+ticks = []
+for i in range(stopA + 1, stopB+1):
+    ticks.append('stop%d'%i)
+
+ind = numpy.arange(N)    # the x locations for the groups
+width = 0.35       # the width of the bars: can also be len(x) sequence
+
+p1 = plt.bar(ind, StopMeans,   width, color='r')
+
+
+plt.ylabel('crowding')
+plt.title('crowding between stop')
+plt.xticks(ind+width/2., ticks )
+plt.yticks(numpy.arange(0,max*1.33,max/6.0))
+#plt.legend( (p1[0]), ('Crowding'),loc = 2)
+
+plt.show()
+
 
 
 #sql = """select * FROM `dataproject`.`transit` limit 200"""
