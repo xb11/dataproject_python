399099e92390fdc33cb7d3d42511d707c7ef8c1b
Bo Xiong <wo635627885@163.com>
2014-08-14 02:16:16 -0400

    maximum of the adjacent stop crowding

--- crowding_histogram.py
+++ crowding_histogram.py
@@ -12,6 +12,7 @@ user_id = "root"
 password = "1234"
 database = "dataproject"
 port = "3308"
+host = "192.168.2.107"
 try:
   cnx = mysql.connector.connect(user=user_id,password=password,database=database,port=port)
 except mysql.connector.Error as err:
@@ -50,14 +51,16 @@ except mysql.connector.Error as err:
         
 
 
-startmonth = 9
+startyear = 2012
+startmonth =9
+endyear = 2013
 endmonth = 10
 starttime = 6
 endtime = 12
-stopA = 3
-QstopA = ""
-QstopB = ""
-stopB = 7
+stopA = 9
+QstopA = "  E19790"#"  S48270"
+stopB = 15
+QstopB = "  E19310"#"  E41250"
 route = 614
 
 
@@ -66,40 +69,51 @@ route = 614
 #first we calculate a value named Dcorwd:LOAD/CAPACITY of a section between the adjacent stop  
 #then based on the exact TRIP of one day,we select the maximum of the Dcrowd between the stopA and
 #stopB which are specified by user
-def calculate_crowding(startmonth,endmonth,starttime,endtime,stopA,stopB,route):
-    sql = "SELECT `LOAD_NUM`,`CAPACITY`,`STOPA`,`daymoyr`,`TRIPA`,`DLMILES` FROM `dataproject`.`transit` \
-    WHERE %d<=HR AND HR<%d AND '2013-%d-01'<=daymoyr AND daymoyr<='2013-%d-31'\
-    AND ROUTE = %d AND %d < STOPA AND STOPA <= %d order by daymoyr,TRIPA ASC"\
-    % (starttime,endtime,startmonth,endmonth,route,stopA,stopB)
+def calculate_crowding(startyear,startmonth,endyear,endmonth,starttime,endtime,stopA,QstopA,stopB,QstopB,route):
+    sql = "SELECT `LOAD_NUM`,`CAPACITY`,`STOPA`,`daymoyr`,`TRIPA`,`DLMILES`,`QSTOPA` FROM `dataproject`.`transit` \
+    WHERE %d<=HR AND HR<%d AND '%d-%d-01'<=daymoyr AND daymoyr<='%d-%d-31'\
+    AND ROUTE = %d AND %d <= STOPA AND STOPA <= %d order by daymoyr,TRIPA,STOPA ASC"\
+    % (starttime,endtime,startyear,startmonth,endyear,endmonth,route,stopA,stopB)
     cursor.execute(sql)
    
-    number = (endmonth-startmonth)*31 +31
+    number = (endmonth-startmonth+1+12*(endyear-startyear))*31 
     crowding = [[] for i in range(number)]
     row = cursor.fetchone()
+    while row[6] != QstopA:
+        row = cursor.fetchone()
     daymoyr = row[3]
     TRIPA = row[4]
     Dcrowd = row[0]*1.0/row[1]
     results = cursor.fetchall()
+    count = 0
     j = 0
     for row in results:
         if row[3] == daymoyr:
             if row[4] == TRIPA:
+                count = count+1
                 if row[0]*1.0/row[1] > Dcrowd:
                     Dcrowd = row[0]*1.0/row[1]                    
             else:
+                if count == stopB - stopA:
+                    crowding[j].append(Dcrowd)
+                if row[6] == QstopA:
+                    TRIPA = row[4]
+                    Dcrowd = row[0]*1.0/row[1]
+                count = 0
+        else:
+            if count == stopB - stopA:
                 crowding[j].append(Dcrowd)
+            #print j
+            if row[6] == QstopA:
+                j = j+1
+                daymoyr = row[3]
                 TRIPA = row[4]
                 Dcrowd = row[0]*1.0/row[1]
-        else:
-            crowding[j].append(Dcrowd)
-            #print j
-            j = j+1
-            daymoyr = row[3]
-            TRIPA = row[4]
-            Dcrowd = row[0]*1.0/row[1]
+            count = 0    
 #    if j+1 < number:
 #        for i in range(j+1,number):
 #            del crowding[i]                        
+    print j+1
     return crowding
 
 #define a function to calculate crowding based on the database user-specified,
@@ -147,16 +161,17 @@ def calculate_crowding2(startmonth,endmonth,starttime,endtime,stopA,stopB,route)
     return crowding
 
 
-crowd_1 = calculate_crowding2(startmonth,endmonth,starttime,endtime,stopA,stopB,route)
-crowd_2 = []
+crowd_1 = calculate_crowding(startyear,startmonth,endyear,endmonth,starttime,endtime,stopA,QstopA,stopB,QstopB,route)
 
-#for i in range(len(crowd_1)):
-#    if len(crowd_1[i]) >0:
-#        crowd_2.append(numpy.mean(crowd_1[i]))
+crowd_2 = []
 
 for i in range(len(crowd_1)):
     if len(crowd_1[i]) >0:
-        crowd_2.extend(crowd_1[i])
+        crowd_2.append(numpy.mean(crowd_1[i]))
+
+#for i in range(len(crowd_1)):
+#    if len(crowd_1[i]) >0:
+#        crowd_2.extend(crowd_1[i])
 
 print crowd_2 
 upper_bound = math.ceil(numpy.max(crowd_2)*10)
@@ -164,8 +179,8 @@ lower_bound = math.floor(numpy.min(crowd_2)*10)
 x = numpy.arange(lower_bound/10,(upper_bound+0.5)/10,0.05)
 
 n, bins, patches = plt.hist(crowd_2,bins = x, normed=0, facecolor='green', alpha=0.5)
-threshold_lower = 0.1
-threshold_upper = 0.3
+threshold_lower = 0.2
+threshold_upper = 0.4
 print n
 percentage = numpy.sum(n[int((threshold_lower-bins[0])*20):int((threshold_upper-bins[0])*20)])*1.0/numpy.sum(n)
 
@@ -176,36 +191,6 @@ plt.xlabel('crowding')
 plt.ylabel('count')
 
 plt.show()
-
-##StopMeans = [0.174056,0.153506,0.164026]
-#N = stopB - stopA
-#max = numpy.max(StopMeans)
-##print max
-##print numpy.arange(0,max*1.33,max/6.0)
-##ticks = ['stop3','stop4','stop5']
-#ticks = []
-#for i in range(stopA + 1, stopB+1):
-#   ticks.append('stop%d'%i)
-#
-#ind = numpy.arange(N)    # the x locations for the groups
-#width = 0.35       # the width of the bars: can also be len(x) sequence
-#
-#p1 = plt.bar(ind, StopMeans,   width, color='r')
-#
-#
-#plt.ylabel('crowding')
-#plt.title('crowding between stop')
-#plt.xticks(ind+width/2., ticks )
-#plt.yticks(numpy.arange(0,max*1.33,max/6.0))
-##plt.legend( (p1[0]), ('Crowding'),loc = 2)
-#
-#plt.show()
-#
-#
-#sql = """select * FROM `dataproject`.`transit` limit 200"""
-#cursor.execute(sql)
-#results = cursor.fetchall()
-#print results[0]
 
 cnx.close()
 
